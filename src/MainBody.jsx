baa4ab319b2f27e9ef4ff73454c11b588096d3f1
Shourya <shourya124sensei@gmail.com>
2025-07-03 19:31:31 +0530

    Changing font style

--- src/MainBody.jsx
+++ src/MainBody.jsx
@@ -2,7 +2,7 @@ function MainBody(props) {
     return(
         <>
         <div className="main-body">
-            <h2>Good Morning {props.name}</h2>
+            <h2>Good Morning {props.name} </h2>
             <div className="main-body-input">
             <textarea type="text" placeholder="Enter your Task"/>
         </div>
